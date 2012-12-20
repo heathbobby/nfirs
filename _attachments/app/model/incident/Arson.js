79ee080db3b11a0b06505d277dc85515401f4eb5
tacshooter <cfdev@fergusonhouse.com>
2012-12-20 11:11:42 -0600

    add obj to arson model

--- _attachments/app/model/incident/Arson.js
+++ _attachments/app/model/incident/Arson.js
@@ -2,6 +2,7 @@ Ext.define('AM.model.incident.Arson', {
     extend: 'Ext.data.Model',
     fields: [
 	 {name:'recordType', type:'int', defaultValue:1100},
-	 {name:'transactionType', type:'int'}
+	 {name:'transactionType', type:'int'},
+	 {name:'test', type:'int'}
 	]
 });
